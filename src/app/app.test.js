a09e13d13402a0773fd8693202b917bb0e02830c
Levi Bernard <levichristianbernard@gmail.com>
2018-10-17 17:42:55 -0600

    updated test components, exported none-connected components

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { NavBar } from './navbar';
+import { App } from './app';
 
-describe('<NavBar />', () => {
+describe('<App />', () => {
     it('Renders without crashing', () => {
-        shallow(<NavBar />);
+        shallow(<App />);
     });
 });
--- src/guess/guess-form.test.js
+++ src/guess/guess-form.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import GuessForm from './guess-form';
+import { GuessForm } from './guess-form';
 
 describe('<GuessForm />', () => {
     it('Renders without crashing', () => {
